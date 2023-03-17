06e61ff7e8b0075210f4eeda261a1e97184eb098
unknown <minhquanctm@gmail.com>
2023-03-17 15:02:53 +0700

    a

---
+++ src/rikkei_academy/AbsoluteNumberCalculator.java
@@ -0,0 +1,9 @@
+package rikkei_academy;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        if (number < 0)
+            return -1;
+        return number;
+    }
+}
